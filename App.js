9da6169e0f31c6c24695f34a23b7e905ebbbb298
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-27 21:23:48 +0530

    Fix issuess

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { StatusBar } from "expo-status-bar";
 import store from "./Store/store";
 import Main from "./Screen/Main/Main";
-import Loader from "./Components/Loader/Loader";
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsReady(true);
-    }, 2000);
-  }, []);
-
-  if (!isReady) {
-    return <Loader />;
-  }
-
   return (
     <Provider store={store}>
       <Main />
